afa8a3dba82df83f0e9e3e1891c5a73788ea02c7
NZO-GB <enzogbrito2@gmail.com>
2026-04-25 22:47:34 +0100

    added basic readiness endpoint

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ func main() {
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("./"))
-	mux.Handle("/", fs)
+	mux.Handle("/app/", http.StripPrefix("/app/",  fs))
 	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`OK`))
+	})
+
 	s := &http.Server{
 		Handler: mux,
 		Addr: ":8080",
